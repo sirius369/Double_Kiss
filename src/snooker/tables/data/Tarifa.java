e515849262032f9cddaf7f284f3bf099f467b372
velid <vimsirovic1@etf.unsa.ba>
2022-05-13 00:26:57 +0200

    Dodani getteri i setteri za atribute

--- src/snooker/tables/data/Tarifa.java
+++ src/snooker/tables/data/Tarifa.java
@@ -1,7 +1,38 @@
 package snooker.tables.data;
 
+import java.util.ArrayList;
+
 public class Tarifa {
     private double cijena;
     private String naziv;
+
+    public Tarifa(String naziv, double cijena){
+        this.naziv = naziv;
+        this.cijena = cijena;
+    }
+
+
+    public double getCijena() {
+        return cijena;
+    }
+
+    public void setCijena(double cijena) {
+        this.cijena = cijena;
+    }
+
+    public String getNaziv() {
+        return naziv;
+    }
+
+    public void setNaziv(String naziv) {
+        this.naziv = naziv;
+    }
+
+    public static ArrayList<Tarifa> mockupTarife(){
+        ArrayList<Tarifa> t = new ArrayList<>();
+        t.add(new Tarifa("Tarifa 1", 2.1));
+        t.add(new Tarifa("Tarifa 2", 2.5));
+        return t;
+    }
 
 }
