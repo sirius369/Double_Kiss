abb11251b5217e37873e8733806a100b093cbf52
velid <vimsirovic1@etf.unsa.ba>
2022-05-13 00:43:27 +0200

    Dodavanje nove tarife u tabelu (i dizajn malo izmijenjen)

--- src/snooker/tables/controller/SettingsController.java
+++ src/snooker/tables/controller/SettingsController.java
@@ -5,8 +5,10 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 import snooker.tables.data.Tarifa;
+
 import java.util.ArrayList;
 
 public class SettingsController {
@@ -14,6 +16,7 @@ public class SettingsController {
     public TableColumn<Tarifa, Double> tarifaCijena;
     public TableView<Tarifa> tarifeTbl;
     public ArrayList<Tarifa> tarife = Tarifa.mockupTarife();
+    public TextField nazivTarifeFld, cijenaTarifeFld;
     @FXML
     public void initialize(){
         System.out.println("Ovamo bi trebalo da dodje");
@@ -24,6 +27,20 @@ public class SettingsController {
 
     public void ukloniTarifu(ActionEvent event){
         tarife.remove(tarifeTbl.getSelectionModel().getSelectedItem());
+        tarifeTbl.setItems(FXCollections.observableArrayList(tarife));
+    }
+
+    public void dodajTarifu(ActionEvent event){
+        if (nazivTarifeFld.getText() == "" || cijenaTarifeFld.getText() == "")
+            return;
+        String naziv = nazivTarifeFld.getText();
+        Double cijena;
+        try {
+            cijena = Double.parseDouble(cijenaTarifeFld.getText());
+        } catch(Exception e){
+            return;
+        }
+        tarife.add(new Tarifa(naziv, cijena));
         tarifeTbl.setItems(FXCollections.observableArrayList(tarife));
     }
 }
