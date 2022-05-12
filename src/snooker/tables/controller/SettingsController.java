e395928f4870feb0aec5f502f43fb194118fef7e
velid <vimsirovic1@etf.unsa.ba>
2022-05-13 01:00:45 +0200

    Izmijena odabrane tarife

--- src/snooker/tables/controller/SettingsController.java
+++ src/snooker/tables/controller/SettingsController.java
@@ -17,12 +17,39 @@ public class SettingsController {
     public TableView<Tarifa> tarifeTbl;
     public ArrayList<Tarifa> tarife = Tarifa.mockupTarife();
     public TextField nazivTarifeFld, cijenaTarifeFld;
+
+    private int odabranaTarifa = -1;
     @FXML
     public void initialize(){
         System.out.println("Ovamo bi trebalo da dodje");
         tarifaNaziv.setCellValueFactory(new PropertyValueFactory<>("naziv"));
         tarifaCijena.setCellValueFactory(new PropertyValueFactory<>("cijena"));
         tarifeTbl.setItems(FXCollections.observableArrayList(tarife));
+        tarifeTbl.getSelectionModel().selectedItemProperty().addListener((obs, oldSelection, newSelection) -> {
+            System.out.println("OVAMO SMO");
+            if (newSelection != null) {
+                odabranaTarifa = tarife.indexOf(newSelection);
+                nazivTarifeFld.setText(tarifeTbl.getSelectionModel().getSelectedItem().getNaziv());
+                Double cijena = (tarifeTbl.getSelectionModel().getSelectedItem().getCijena());
+                cijenaTarifeFld.setText(cijena.toString());
+            }
+        });
+    }
+
+    public void izmijeniTarifu(ActionEvent event){
+        if (odabranaTarifa == -1)
+            return;
+        Double cijena;
+        try {
+            cijena = Double.parseDouble(cijenaTarifeFld.getText());
+        } catch(Exception e){
+            return;
+        }
+        if (tarife.size() <= odabranaTarifa)
+            return;
+        tarife.get(odabranaTarifa).setNaziv(nazivTarifeFld.getText());
+        tarife.get(odabranaTarifa).setCijena(cijena);
+        tarifeTbl.refresh();
     }
 
     public void ukloniTarifu(ActionEvent event){
@@ -34,6 +61,9 @@ public class SettingsController {
         if (nazivTarifeFld.getText() == "" || cijenaTarifeFld.getText() == "")
             return;
         String naziv = nazivTarifeFld.getText();
+        for (var t : tarife)
+            if (t.getNaziv().equals(naziv))
+                return;
         Double cijena;
         try {
             cijena = Double.parseDouble(cijenaTarifeFld.getText());
